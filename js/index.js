dcd78eb342fb42df9b017fef5b3cd1cbdb728fad
sebast4an <szczypinskisebastian@gmail.com>
2021-04-24 18:11:54 +0200

    repairing: pricing box and hover navigation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const navButton = document.querySelector('.navbutton');
+const panel = document.querySelector('.navigation__list');
 
 navButton.addEventListener(`click`, () => {
     navButton.classList.toggle('navbutton--active');
+    panel.classList.toggle('navigation__list--active');
 });
